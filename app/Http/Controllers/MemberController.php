fffd72c14a217ab7db61f78613789bb9f265d5ab
Zhao-666 <2268699370@qq.com>
2018-03-13 00:01:10 +0800

    create

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 2018/3/12
+ * Time: 23:35
+ */
+
+namespace App\Http\Controllers;
+
+
+class MemberController extends Controller
+{
+    public function info(){
+        return 'member-info';
+    }
+}
